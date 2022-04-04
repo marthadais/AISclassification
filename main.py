d8e1c46481deccee357768ffbde0a74de8dd264d
marthadais <dais.martha@gmail.com>
2022-04-04 12:25:20 -0300

    pos-processing

--- main.py
+++ main.py
@@ -2,6 +2,38 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from preprocessing import features as fts
 import os
+from itertools import groupby
+from collections import Counter
+
+
+def posprocessing(x, min_points=1, verbose=False):
+    """
+    It applies a pos-processing on the clustering labels.
+    It changes the label of the current observation to the previous one
+     when the current observation and the next ones of same label have less
+     than a minimum number
+    :param x: labels sequence (array)
+    :param min_points: minimum number in the sequence to change the label
+    :param verbose: if True, it will print something
+    :return: the new list of labels
+    """
+    lbl = x.tolist()
+    new_list = []
+    c=0
+    for k, g in groupby(lbl):
+        s = list(g)
+        if verbose:
+            print(f'{c} of {len(lbl)}')
+        if len(s) == min_points:
+            if len(new_list) == 0:
+                new_list = new_list + [0]
+            else:
+                new_list = new_list + [new_list[-1]]
+        else:
+            new_list = new_list + s
+        c = c+1
+
+    return new_list
 
 print('Reading Dataset')
 data_file = f'./data/preprocessed/DCAIS_[30_ 1001_ 1002]_None-mmsi_region_[46_ 51_ -130_ -122.5]_01-04_to_30-06_trips.csv'
@@ -9,15 +41,14 @@ dataset = pd.read_csv(data_file, parse_dates=['time'], low_memory=False)
 dataset['time'] = dataset['time'].astype('datetime64[ns]')
 dataset = dataset.sort_values(by=['trajectory', "time"])
 
-window = 5
-n_dirs = 16
-seconds = 10*60
-features_path = f'./data/features_win_{window}_rose_{n_dirs}_time_win_{seconds}.csv'
+seconds = 10*60 # window seconds
+features_path = f'./data/features_window_10.csv'
 if not os.path.exists(features_path):
-    features_all = fts.get_features(dataset, n_dirs=n_dirs, win=window, eps=seconds)
-    features_all.to_csv(features_path, index=False)
+    # features_all = fts.get_all_features(dataset, n_dirs=n_dirs, win=window, eps=seconds)
+    features = fts.get_features(dataset, eps=seconds)
+    features.to_csv(features_path, index=False)
 else:
-    features_all = pd.read_csv(features_path)
+    features = pd.read_csv(features_path)
 
 # select features to run the clustering
 # features columns:'sog', 'cog', 'rose',
@@ -29,20 +60,21 @@ else:
 # [str(i) for i in range(n_dirs)]
 
 #%%
-cols = ['ma_t_sog', 'ms_t_sog', 'msum_t_roc']
-data_cl = features_all[cols]
+data_cl = features[['ma_sog', 'msum_roc']]
 
 print('Clustering')
 # Clustering
 nc = 10
 model = KMeans(nc).fit(data_cl)
 labels = model.labels_
-# metrics.silhouette_score(data_cl, labels)
-data_cl['label'] = labels
-dataset['labels'] = labels
+
+labels2 = posprocessing(labels)
+data_cl['labels'] = labels2
+dataset['labels'] = labels2
+
 
 # Saving
 # index of a few trajectories to quickly evaluate
 test = dataset[dataset['trajectory'].isin([213, 117, 145, 26, 11])]
-test.to_csv(f'fishing_5_{nc}_{window}_{cols}.csv', index=False)
-dataset.to_csv(f'fishing_{nc}_{window}_{cols}.csv', index=False)
+test.to_csv(f'fishing_5_{nc}_{seconds}.csv', index=False)
+dataset.to_csv(f'fishing_{nc}_{seconds}.csv', index=False)
--- preprocessing/features.py
+++ preprocessing/features.py
@@ -26,7 +26,7 @@ def diff_cog(x):
     return diff_x
 
 
-def MA_MS_simple(data, col='sog', window=30):
+def MA_MS_simple(data, col='sog', window=30, stats='mean'):
     data_agg = pd.DataFrame()
     for traj_id in data['trajectory'].unique():
         trajectory = data[data['trajectory'] == traj_id][col]
@@ -36,12 +36,16 @@ def MA_MS_simple(data, col='sog', window=30):
         if window % 2 == 0:
             aux2 = aux2[0:-1]
         trajectory = pd.concat([aux, trajectory, aux2])
+        trajectory.reset_index(drop=True)
 
         # Moving average
         traj_roll = pd.DataFrame()
-        traj_roll[f'ma_{col}'] = trajectory.rolling(window, center=True).mean()
-        traj_roll[f'ms_{col}'] = trajectory.rolling(window, center=True).std()
-        traj_roll[f'msum_{col}'] = trajectory.rolling(window, center=True).sum()
+        if stats == 'std':
+            traj_roll[f'ms_{col}'] = trajectory.rolling(window, center=True).std()
+        elif stats == 'sum':
+            traj_roll[f'msum_{col}'] = trajectory.rolling(window, center=True).sum()
+        else:
+            traj_roll[f'ma_{col}'] = trajectory.rolling(window, center=True).mean()
         traj_roll = traj_roll.dropna()
         if traj_roll.shape[0] == 0:
             print(f'Trajectory {traj_id} has not enough observations for window size {window}!')
@@ -51,7 +55,7 @@ def MA_MS_simple(data, col='sog', window=30):
     return data_agg
 
 
-def MA_MS_timestamp(data, col='sog', epsilon=180, verbose=True):
+def MA_MS_timestamp(data, col='sog', epsilon=180, stats='mean', verbose=True):
     data_agg = pd.DataFrame()
     mean_window = []
     if verbose:
@@ -64,37 +68,54 @@ def MA_MS_timestamp(data, col='sog', epsilon=180, verbose=True):
         trajectory_time = data[data['trajectory'] == traj_id]['time']
         delta_T = trajectory_time.diff()
 
-        traj_roll_mean = []
-        traj_roll_std = []
-        traj_roll_sum = []
+        traj_roll = []
         for obs in range(trajectory.shape[0]):
-            step = 1
-            time_in_w = timedelta(0)
-            while (time_in_w.seconds <= epsilon) and obs+step < trajectory.shape[0]:
-                time_in_w = time_in_w + delta_T.iloc[obs+step]
+            # get start point
+            step = 0
+            time_in_w = delta_T.iloc[obs]
+            while (time_in_w.seconds <= epsilon//2) and (obs-(step+1) >= 0):
                 step = step + 1
-            end = obs + step-1
-            if np.isnan(trajectory.iloc[obs:end].mean()):
+                time_in_w = time_in_w + delta_T.iloc[obs-step]
+            ini = obs - (step - 1)
+            if step == 0:
+                ini = obs
+            # get end point
+            step = 0
+            time_in_w = timedelta(0)
+            while (time_in_w.seconds <= epsilon // 2) and (obs+(step+1) < trajectory.shape[0]):
+                step = step + 1
+                time_in_w = time_in_w + delta_T.iloc[obs+step]
+            end = obs + (step - 1)
+            if step == 0:
+                end = obs
+            if np.isnan(trajectory.iloc[ini:end].mean()):
                 end = end + 1
-            traj_roll_mean.append(trajectory.iloc[obs:end].mean())
-            traj_roll_std.append(trajectory.iloc[obs:end].std())
-            traj_roll_sum.append(trajectory.iloc[obs:end].sum())
+            if stats == 'std':
+                traj_roll.append(trajectory.iloc[ini:end].std())
+            elif stats == 'sum':
+                traj_roll.append(trajectory.iloc[ini:end].sum())
+            else:
+                traj_roll.append(trajectory.iloc[ini:end].mean())
             mean_window_traj.append(step)
 
-        traj_roll = pd.DataFrame()
-        traj_roll[f'ma_t_{col}'] = pd.DataFrame(traj_roll_mean)
-        traj_roll[f'ms_t_{col}'] = pd.DataFrame(traj_roll_std)
-        traj_roll[f'msum_t_{col}'] = pd.DataFrame(traj_roll_sum)
-        data_agg = pd.concat([data_agg, traj_roll], axis=0)
+        trajectory_roll = pd.DataFrame()
+        if stats == 'std':
+            trajectory_roll[f'ms_t_{col}'] = pd.DataFrame(traj_roll)
+        elif stats == 'sum':
+            trajectory_roll[f'msum_t_{col}'] = pd.DataFrame(traj_roll)
+        else:
+            trajectory_roll[f'ma_t_{col}'] = pd.DataFrame(traj_roll)
+        data_agg = pd.concat([data_agg, trajectory_roll], axis=0)
         mean_window.append(np.array(mean_window_traj).mean())
 
+    data_agg = data_agg.fillna(0)
     if verbose:
         print(f'\t time window: {np.array(mean_window).mean()}, {np.array(mean_window).std()}')
     data_agg = data_agg.reset_index(drop=True)
     return data_agg
 
 
-def get_features(data, n_dirs=4, win=30, eps=180):
+def get_all_features(data, n_dirs=4, win=30, eps=180):
     features = remove_short_trajectories(data)
     features = features[['trajectory', 'time', 'sog', 'cog']]
     features['rose'] = cog_windrose(features['cog'], n_dirs=n_dirs)
@@ -122,6 +143,34 @@ def get_features(data, n_dirs=4, win=30, eps=180):
     MA_data = MA_MS_simple(features, col='acceleration', window=win)
     features = pd.concat([features, MA_data], axis=1)
     MA_data = MA_MS_simple(features, col='roc', window=win)
+    features = pd.concat([features, MA_data], axis=1)
+
+    return features
+
+
+def get_features_time(data, eps=180):
+    features = remove_short_trajectories(data)
+    features = features[['trajectory', 'time', 'sog', 'cog']]
+
+    features['roc'] = diff_cog(features['cog'])
+    features = features.fillna(0)
+    MA_data = MA_MS_timestamp(features, col='sog', epsilon=eps, stats='mean')
+    features = pd.concat([features, MA_data], axis=1)
+    MA_data = MA_MS_timestamp(features, col='roc', epsilon=eps, stats='sum')
+    features = pd.concat([features, MA_data], axis=1)
+
+    return features
+
+
+def get_features(data, win=10):
+    features = remove_short_trajectories(data)
+    features = features[['trajectory', 'time', 'sog', 'cog']]
+
+    features['roc'] = diff_cog(features['cog'])
+    features = features.fillna(0)
+    MA_data = MA_MS_simple(features, col='sog', window=win)
+    features = pd.concat([features, MA_data], axis=1)
+    MA_data = MA_MS_simple(features, col='roc', window=win, stats='sum')
     features = pd.concat([features, MA_data], axis=1)
 
     return features
