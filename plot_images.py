47238aa22e16ce8214642c96a57e4518fb906a35
marthadais <dais.martha@gmail.com>
2022-05-16 13:52:49 -0300

    fixing plot files

--- plot_images.py
+++ plot_images.py
@@ -7,36 +7,36 @@ nc = 8
 colors_bi = np.array(['wheat', 'blue'])
 cmap = plt.cm.get_cmap('Accent')
 
-# print('Getting Time-based Features')
-# seconds = 10*60
-# folder = './results/time_final/'
-# features_path = f'{folder}/data/features_window_time_{seconds}.csv'
-# features = pd.read_csv(features_path)
-# data_cl_time = features[['ma_t_acceleration', 'msum_t_roc']]
-# file_name = f'{folder}/fishing_{nc}_{seconds}.csv'
-# dataset_time = pd.read_csv(file_name)
-#
-#
-# fig = plt.figure(figsize=(10, 9))
-# plt.scatter(data_cl_time['ma_t_acceleration'], data_cl_time['msum_t_roc'], c=colors_bi[dataset_time['labels_pos']], alpha=0.7)
-# plt.xlabel('MA of acceleration', fontsize=15)
-# plt.ylabel('MS of ROC', fontsize=15)
-# plt.xticks(np.arange(-1.5, 1.7, step=0.2), fontsize=15)
-# plt.yticks(np.arange(-800, 1600, step=100), fontsize=15)
-# plt.grid(True)
-# plt.tight_layout()
-# # plt.show()
+print('Getting Time-based Features')
+seconds = 10*60
+folder = './results/time_final/'
+features_path = f'{folder}/data/features_window_time_{seconds}.csv'
+features = pd.read_csv(features_path)
+data_cl_time = features[['ma_t_acceleration', 'msum_t_roc']]
+file_name = f'{folder}/fishing_{nc}_{seconds}.csv'
+dataset_time = pd.read_csv(file_name)
+
+
+fig = plt.figure(figsize=(10, 9))
+plt.scatter(data_cl_time['ma_t_acceleration'], data_cl_time['msum_t_roc'], c=colors_bi[dataset_time['labels_pos']], alpha=0.7)
+plt.xlabel('MA of acceleration', fontsize=15)
+plt.ylabel('MS of ROC', fontsize=15)
+plt.xticks(np.arange(-1.5, 1.7, step=0.2), fontsize=15)
+plt.yticks(np.arange(-800, 1600, step=100), fontsize=15)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 # plt.savefig(f'./results/images/time_scatter.png', bbox_inches='tight')
-#
-# fig = plt.figure(figsize=(10, 9))
-# plt.scatter(data_cl_time['ma_t_acceleration'], data_cl_time['msum_t_roc'], c=cmap(dataset_time['labels']), alpha=0.7)
-# plt.xlabel('MA of acceleration', fontsize=15)
-# plt.ylabel('MS of ROC', fontsize=15)
-# plt.xticks(np.arange(-1.5, 1.7, step=0.2), fontsize=15)
-# plt.yticks(np.arange(-800, 1600, step=100), fontsize=15)
-# plt.grid(True)
-# plt.tight_layout()
-# # plt.show()
+
+fig = plt.figure(figsize=(10, 9))
+plt.scatter(data_cl_time['ma_t_acceleration'], data_cl_time['msum_t_roc'], c=cmap(dataset_time['labels']), alpha=0.7)
+plt.xlabel('MA of acceleration', fontsize=15)
+plt.ylabel('MS of ROC', fontsize=15)
+plt.xticks(np.arange(-1.5, 1.7, step=0.2), fontsize=15)
+plt.yticks(np.arange(-800, 1600, step=100), fontsize=15)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 # plt.savefig(f'./results/images/time_scatter_{nc}.png', bbox_inches='tight')
 
 
@@ -91,7 +91,6 @@ plt.plot(index_d, time_d, marker="p", linestyle="-", linewidth=2,
 plt.plot(index_d, obs_d, marker="p", linestyle="-", linewidth=2,
          markersize=7, label='message-based')
 # plt.plot(index_d, np.repeat(0.5, len(obs_d)), color='black', linestyle="-", linewidth=2, label='threshold')
-plt.title(f'K-means - {iter} obs')
 plt.ylabel('DBI', fontsize=15)
 plt.xlabel('Number of clusters', fontsize=15)
 plt.legend(fontsize=20)
